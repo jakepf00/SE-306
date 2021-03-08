52b197d603920a9d23eb7483091e7ed6d27dd6ee
Jacob Pfeifer <jakepf00@gmail.com>
2021-03-08 13:25:14 -0600

    added a function to update the equipment list based on data received from database

--- BITS/Website/js/equipmentManagement.js
+++ BITS/Website/js/equipmentManagement.js
@@ -17,6 +17,17 @@ function updateWebpage() {
         console.log(equipmentTable);
         // from here, edit the webpage to display received data
         // loop through table, get each element name, add to list
+        // -> will append to a string that will eventually be used in the HTML 
+        var equipmentResults = document.getElementById("equipment_results");
+        var newEquipmentList = "";
+        
+        equipmentTable.forEach(function(item) {
+            newEquipmentList = newEquipmentList.concat("<li><a href=\"#\">", item.aoeu, "</li></a>");
+            // TODO: make list be filtered by search string
+            // TODO: make links open a popup with information about the equipment and a way to rent it
+        });
+        
+        equipmentResults.innerHTML = newEquipmentList;
     }
     else {
         setTimeout(updateWebpage, 100); // wait 100ms then try again
@@ -27,4 +38,7 @@ function displayEquipment() {
     console.log('Inside displayEquipment()');
     getEquipmentTable();
     updateWebpage();
+    equipmentTable = null; // maybe don't want to do this?
+        // don't want variable to be saved over between searches, because then upon new search, function might use old data before it is updated
+        // but we might want to use it later to display the data for a specific equipment if it is clicked
 }
