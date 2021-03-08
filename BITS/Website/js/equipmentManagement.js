0cdc8f64d9439043c79580d6f53f55f58f7465f1
Jacob Pfeifer <jakepf00@gmail.com>
2021-03-08 13:43:44 -0600

    Added a variable in updateWebpage to access the value of the search string

--- BITS/Website/js/equipmentManagement.js
+++ BITS/Website/js/equipmentManagement.js
@@ -1,5 +1,3 @@
-//var equipmentTable;
-
 function getEquipmentTable() {    
     var apiUrl = 'https://localhost:44335/api/Table1';
     fetch(apiUrl).then(response => {
@@ -19,11 +17,13 @@ function updateWebpage(equipmentTable) {
     // loop through table, get each element name, add to list
     // -> will append to a string that will eventually be used in the HTML 
     var equipmentResults = document.getElementById("equipment_results");
+    var searchString = document.getElementById("search_string").value;
+    console.log(searchString);
     var newEquipmentList = "";
     
     equipmentTable.forEach(function(item) {
         newEquipmentList = newEquipmentList.concat("<li><a href=\"#\">", item.aoeu, "</li></a>");
-        // TODO: make list be filtered by search string
+        // TODO: make list be filtered by search string -> add code for if it didn't match any results
         // TODO: make links open a popup with information about the equipment and a way to rent it
     });
     
