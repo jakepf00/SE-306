9ef2e7685ba0d82ea7a16775602514f803bdbe0d
Jacob Pfeifer <jakepf00@gmail.com>
2021-03-08 12:55:22 -0600

    Added code to wait for database response before editing the webpage

--- BITS/Website/js/equipmentManagement.js
+++ BITS/Website/js/equipmentManagement.js
@@ -1,17 +1,30 @@
-function getEquipment() {    
+var equipmentTable;
+
+function getEquipmentTable() {    
     var apiUrl = 'https://localhost:44335/api/Table1';
     fetch(apiUrl).then(response => {
         return response.json();
     }).then(data => {
         // Work with JSON data here
-        console.log(data);
-        
+        equipmentTable = data;
     }).catch(err => {
         // Do something for an error here
     });
 }
 
+function updateWebpage() {
+    if (equipmentTable) { // wait until the getEquipmentTable function has completed before updating webpage with recieved data
+        console.log(equipmentTable);
+        // from here, edit the webpage to display received data
+        // loop through table, get each element name, add to list
+    }
+    else {
+        setTimeout(updateWebpage, 100); // wait 100ms then try again
+    }
+}
+
 function displayEquipment() {
-    console.log('Calling displayEquipment()');
-    getEquipment();
+    console.log('Inside displayEquipment()');
+    getEquipmentTable();
+    updateWebpage();
 }
