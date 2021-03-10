0fcb40dd96e8f0624cf1aa7af2c7cc4c014d0597
Jacob Pfeifer <jakepf00@gmail.com>
2021-03-10 16:39:14 -0600

    Added elements to concession page and created outline for concession javascript file.

---
+++ BITS/WebsiteAdmin/js/concessions.js
@@ -0,0 +1,18 @@
+function displaySKU() {
+    console.log("Inside displaySKU()");
+    // findItemInSystem();
+    // updateWebpage();
+}
+
+function scanSKU() {
+    // get SKU from textbox or external scanner
+}
+
+function findItemInSystem() {
+    // access the database, search for item, return database location
+    // also save database response in a global variable?
+}
+
+function removeItemFromSystem(SKU) {
+    // use given SKU to remove item from database
+}
