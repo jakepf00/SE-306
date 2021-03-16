318d90059163d0a0a1d54bc5afac4c3109e4ab37
Jacob Pfeifer <jakepf00@gmail.com>
2021-03-15 20:32:12 -0500

    Made displaySKU use textbox input and made updateWebpage change some contents of the page

--- BITS/WebsiteAdmin/js/concessions.js
+++ BITS/WebsiteAdmin/js/concessions.js
@@ -1,6 +1,7 @@
 // called when search button is clicked
 function displaySKU() {
-    findItemInSystem("jake      ").then(updateWebpage, updateWebpageFail);
+    var SKU = document.getElementById("search_sku").value;
+    findItemInSystem(SKU).then(updateWebpage, updateWebpageFail);
 }
 
 function removeItemFromInventory() {
@@ -17,22 +18,23 @@ async function findItemInSystem(SKU) {
     // also save database response in a global variable
     // returns -1 if the item is not contained in the database
 
-    var itemLocation
+    var itemLocation;
 
     fetch(apiUrl).then(response => {
         return response.json();
     }).then(data => {
         concessionsTable = data;
         
+        itemLocation = -1;
         for(tableLocation = 0; tableLocation < data.length; tableLocation++) {
             if (data[tableLocation].aoeu == SKU) {
                 itemLocation = tableLocation;
             }
         }
-    
+        console.log(itemLocation);
+        
     }).catch(err => {
         console.log("An error ocurred in retrieving the concessions table from the database");
-        itemLocation -1;
     });
 
     while(typeof itemLocation === "undefined") {
@@ -45,10 +47,19 @@ function removeItemFromSystem(SKU) {
     // use given SKU to remove item from database
 }
 
-function updateWebpage(itemLocation) {
-    console.log(itemLocation)
+function updateWebpage(tableLocation) {
+    var concessionsResults = document.getElementById("concessions_results");
+    var newConcessionsResults = "";
+
+    if (tableLocation != -1) {
+        newConcessionsResults += concessionsTable[tableLocation].aoeu;
+    }
+    else newConcessionsResults = "No item matched the given SKU";
+
+    concessionsResults.innerHTML = newConcessionsResults;
 }
 
 function updateWebpageFail() {
     // called when the promise fails and itemLocation isn't found
+    concessionsResults.innerHTML = "Database error";
 }
