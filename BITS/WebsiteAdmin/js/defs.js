71d441b256966ad83166ec2de706af5d7306dd94
Jacob Pfeifer <jakepf00@gmail.com>
2021-04-05 07:49:27 -0500

    Moved Javascript API access functions into one file

--- BITS/WebsiteAdmin/js/defs.js
+++ BITS/WebsiteAdmin/js/defs.js
@@ -4,7 +4,178 @@ var employeeInfoApiUrl = 'http://127.0.0.1:5000/employeeinfo';
 var equipmentApiUrl = 'http://127.0.0.1:5000/equipment';
 var reservationsApiUrl = 'http://127.0.0.1:5000/reservations';
 
-
 var equipmentTable;
 var concessionsTable;
 var reservationsTable;
+
+// API access functions
+//////////////////////////////////////////////////////// Reservations
+function getReservationsTable() {    
+    fetch(reservationsApiUrl).then(response => {
+        return response.json();
+    }).then(data => {
+        reservationsTable = data;
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+function postReservation(newReservation) {
+    fetch(reservationsApiUrl, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: newReservation
+    }).then(response => {
+        getReservationsTable(); // refresh reservations table after post is complete
+        document.getElementById("reservation_results").innerHTML = "<p>Reservation made successfully</p>";
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+function deleteReservation(ID) {
+    var newUrl = reservationsApiUrl + "/" + ID;
+    fetch(newUrl, {
+        method: 'DELETE',
+    }).then(response => {
+        getReservationsTable(); // refresh reservations table after delete is complete
+        document.getElementById("reservation_results").innerHTML = "<p>Reservation cancelled successfully</p>";
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+function putReservation(updatedReservation) {
+    fetch(reservationsApiUrl, {
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: updatedReservation
+    }).then(response => {
+        getReservationsTable(); // refresh reservations table after put is complete
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+//////////////////////////////////////////////////////// Concessions
+function getConcessionsTable() {
+    fetch(concessionsApiUrl).then(response => {
+        return response.json();
+    }).then(data => {
+        concessionsTable = data;
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+function postConcession() {
+    var newSKU = getMaxSKU() + 1;
+    var newConcession = {
+        "sku": newSKU,
+        "itemName": document.getElementById("new_item_name").value,
+        "cost": parseFloat(document.getElementById("new_item_cost").value),
+        "quantity": parseInt(document.getElementById("new_item_quantity").value),
+        "location": document.getElementById("new_item_location").value,
+    }
+    fetch(concessionsApiUrl, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify(newConcession)
+    }).then(response => {
+        getConcessionsTable(); // refresh concessions table after post is complete
+        document.getElementById("concessions_results").innerHTML = "<p>Item created successfully with SKU of " + newSKU + "</p>";
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+function deleteConcessionsItem(SKU) {
+    var newUrl = concessionsApiUrl + "/" + SKU;
+    fetch(newUrl, {
+        method: 'DELETE',
+    }).then(response => {
+        getConcessionsTable(); // refresh concessions table after delete is complete
+        document.getElementById("concessions_results").innerHTML = "Item was successfully removed from database"
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+function putConcession(updatedConcession) {
+    fetch(concessionsApiUrl, {
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: updatedConcession
+    }).then(response => {
+        getConcessionsTable(); // refresh concession table after put is complete
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+//////////////////////////////////////////////////////// Equipment
+function getEquipmentTable() {    
+    fetch(equipmentApiUrl).then(response => {
+        return response.json();
+    }).then(data => {
+        equipmentTable = data;
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+function postEquipment() {
+    var newID = getMaxEquipmentID() + 1;
+    var newEquipment = {
+        "eqId": newID,
+        "name": document.getElementById("new_equipment_name").value,
+        "location": document.getElementById("new_equipment_location").value,
+        "quantity": parseInt(document.getElementById("new_equipment_quantity").value)
+    }
+    fetch(equipmentApiUrl, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify(newEquipment)
+    }).then(response => {
+        getEquipmentTable(); // refresh eq table after post is complete
+        document.getElementById("equipment_results").innerHTML = "Equipment added successfully";
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+function deleteEquipment(eqId) {
+    var newUrl = equipmentApiUrl + "/" + eqId;
+    fetch(newUrl, {
+        method: 'DELETE',
+    }).then(response => {
+        getEquipmentTable(); // refresh eq table after delete is complete
+        document.getElementById("equipment_results").innerHTML = "Equipment removed successfully";
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+function putEquipment(updatedEquipment) {
+    fetch(equipmentApiUrl, {
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: updatedEquipment
+    }).then(response => {
+        getEquipmentTable(); // refresh concessions table after put is complete
+    }).catch(err => {
+        console.log(err);
+    });
+}
